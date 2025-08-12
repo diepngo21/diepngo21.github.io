bc993f6f2d7db11f3a59e0489a428e6b63e2c566
diepngo21 <thuanvsnguyen@gmail.com>
2025-08-12 15:28:22 -0700

    test

--- src/data/info.data.js
+++ src/data/info.data.js
@@ -1,6 +1,6 @@
 export const data = {
   name: "Diep",
-  job: "Software Engineer",
+  job: "Softw2are Engineer",
   phone: "123456789",
   gmail: "diepngo0531@gmail.com",
   linkedin: "https://www.linkedin.com/in/diepngo21",
